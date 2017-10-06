7a207d7d436f6b5824403e51ac689ec2bd1cdd0d
Travis Cotney <tcotney96@gmail.com>
2017-10-06 12:06:04 -0500

    Original Class Creation

---
+++ Team Project/src/team/project/TASConnection.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.sql.*;
+import java.util.*;
+package team.project;
+
+/**
+ *
+ * @author Travis
+ */
+public class TASConnection {
+    private Connection con;
+    private Statement state;
+    private ResultSet set;
+
+    public TastConnection(){
+        try{
+            
+            
+        }catch(Exception e){}
+        
+    }
+    
+}
